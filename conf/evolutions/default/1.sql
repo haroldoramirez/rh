c4eab498587760a84347e5b09e731f9b452988fb
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-08-02 17:43:45 -0300

    configurado com os plugins ebean e dependencias do postgresql

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,18 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table colaborador (
+  id                            bigint not null,
+  nome                          varchar(255),
+  constraint pk_colaborador primary key (id)
+);
+create sequence colaborador_seq;
+
+
+# --- !Downs
+
+drop table if exists colaborador;
+drop sequence if exists colaborador_seq;
+
